52c920b071b10872b7ec4a9f511d5aea6514a3bc
lacsinagramar <lacsinagramar@gmail.com>
2020-12-25 15:56:35 +0800

    task: add database config for staging and prod

--- config/database.js
+++ config/database.js
@@ -1,20 +1,35 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'mongoose',
-      settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'chijeu'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
-      },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+const _ = require('lodash')
+
+module.exports = ({ env }) => {
+  const localSettings = {
+    host: env('DATABASE_HOST', '127.0.0.1'),
+    srv: env.bool('DATABASE_SRV', false),
+    port: env.int('DATABASE_PORT', 27017),
+    database: env('DATABASE_NAME', 'chijeu'),
+    username: env('DATABASE_USERNAME', null),
+    password: env('DATABASE_PASSWORD', null),
+  }
+  
+  const stagingProdSettings = {
+    uri: env('DATABASE_URI'),
+    database: env('DATABASE_NAME', 'chijeu')
+  }
+
+  const isLocalEnv = _.includes(_.lowerCase(env('APP_ENV')), 'local')
+
+  const settings = isLocalEnv ? localSettings : stagingProdSettings
+
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'mongoose',
+        settings,
+        options: {
+          authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+          ssl: env.bool('DATABASE_SSL', false),
+        },
       },
     },
-  },
-});
+  }
+}
